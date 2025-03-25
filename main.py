c13bdcc9a3446b64ce81d370e976101ab0d1eb69
besa-vinceadrian <vinceadrianbesa@gmail.com>
2025-03-25 13:18:51 +0800

    assign tasks

---
+++ main.py
@@ -0,0 +1,26 @@
+# TODO(Salespara): Get the user inputs for the details of the order(s).
+
+def get_order_details():
+    pass
+
+# TODO(Maestre): Get the user inputs for the details of the customer.
+
+def get_customer_details():
+    pass
+
+# TODO(Besa): Calculate the grand total for the order(s).
+
+def calculate_grand_total():
+    pass
+
+# TODO(Serquina): Display the summary of order(s).
+def display_order_summary():
+    pass
+
+#TODO(Bualat): Call the functions in the main function.
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
