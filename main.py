d18bae27ceea163098613cc01619bcf357957f24
salespara-ricagenevive <salespara-ricagenevive>
2025-03-25 14:29:25 +0800

    add function for get_order_details

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 # TODO(Salespara): Get the user inputs for the details of the order(s).
 
 def get_order_details():
-    pass
+    product_names = []
+    product_prices = []
+    product_quantities = []
+
+    while True:
+        product_name = input("Enter Product Name: ")
+        product_price = float(input("Enter Product Price: "))
+        product_quantity = int(input("Enter Product Quantity: "))
+        product_names.append(product_name)
+        product_prices.append(product_price)
+        product_quantities.append(product_quantity)
+        another_item = input("Add another item? (y/n): ").lower()
+        if another_item != 'y':
+            break
+        
+    return product_names, product_prices, product_quantities
 
 # TODO(Maestre): Get the user inputs for the details of the customer.
 
